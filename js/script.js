167e7bfeec92d0d6d8ddedd24d9d581a2f756149
Pallas <pallasravae@yahoo.com>
2015-08-25 13:00:20 -0600

    btn

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){
+
+	function addItemToList(item){
+		var li = '<li class="checkbox"><div class="row"><div class="col-xs-8"><label><input type="checkbox" value="">'+item+'</label></div><div class="col-xs-4"><button class="delete btn btn-warning btn-xs" type="submit">Delete</button></div></div></li>';
+		$("#list").append(li);
+	}
+
+	$(".addition").submit(function(event){
+		addItemToList($(".fooditem").val())
+		event.preventDefault();
+	});
+
+});
+
+
+
+
+
+
+// create a list item in js and set the text to the list item
+// $('<li/>')
+// how to create li in jq
+// append li to ul...?
