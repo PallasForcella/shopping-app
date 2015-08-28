a1443a59a6ee7facb29e803d566d9072458d3308
Pallas <pallasravae@yahoo.com>
2015-08-28 10:37:50 -0600

    favicon

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@
 $(document).ready(function(){
 
 	function addItemToList(item){
-		var li = '<li class="checkbox"><div class="row"><div class="col-xs-8"><label><input type="checkbox" value="">'+item+'</label></div><div class="col-xs-4"><button class="delete btn btn-warning btn-xs" type="submit">Delete</button></div></div></li>';
+		// var li = '<li class="checkbox"><div class="row"><div class="col-xs-8"><label><input type="checkbox" value="">'+item+'</label></div><div class="col-xs-4"><button class="delete btn btn-warning btn-xs" type="submit">Delete</button></div></div></li>';
+		var li = DOM.buildNode({
+	    	el: 'l1',
+	    	kids: [
+	    		{
+			    	el: 'div',
+			    	_className: 'row'
+	    		}
+	    		kids: [
+		    		{
+		    			el:'div',
+		    			_className: 'col-xs-8'
+		    		}
+	    			]
+	    	]
+		});
+	    	]
+		});
 		$("#list").append(li);
 	}
 
@@ -16,6 +33,10 @@ $(document).ready(function(){
 		e.preventDefault();
 		$(this).parent().parent().parent().remove();
 	});	
+
+	var li = DOM.buildNode({
+    	kids: 'li'
+	});
 });
 
 
