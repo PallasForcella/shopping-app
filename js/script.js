96ec475b8329fe3c99507dca5955566922d0242b
Pallas <pallasravae@yahoo.com>
2015-09-01 15:56:04 -0600

    not working

--- js/script.js
+++ js/script.js
@@ -1,50 +1,47 @@
-
-
-$(document).ready(function(){
-
-	function addItemToList(item){
+ $(document).ready(function(){
+ 
+ 	function addItemToList(item){
 		// var li = '<li class="checkbox"><div class="row"><div class="col-xs-8"><label><input type="checkbox" value="">'+item+'</label></div><div class="col-xs-4"><button class="delete btn btn-warning btn-xs" type="submit">Delete</button></div></div></li>';
-		var li = DOM.buildNode({
-	    	el: 'l1',
-	    	kids: [
-	    		{
-			    	el: 'div',
-			    	_className: 'row'
-	    		}
-	    		kids: [
-		    		{
-		    			el:'div',
-		    			_className: 'col-xs-8'
-		    		}
-	    			]
-	    	]
-		});
-	    	]
-		});
-		$("#list").append(li);
-	}
+ 		var li = DOM.buildNode({
 
-	$(".addition").submit(function(event){
-		addItemToList($(".fooditem").val())
-		event.preventDefault();
-	});
+			el: 'li',
+			kids: [{
+				_className: 'row',
+				kids: [
+					{
+						_className: 'col-xs-8',
+						kids: [{
+							el: 'label',
+							kids: [
+								{
+									el: 'input',
+									type: 'checkbox'
+								},
+								item
+							]
+						}]
+					},
+					{
+						_className: 'col-xs-4',
+						kids: [{
+							el: 'button',
+							_className: 'delete btn btn-warning btn-xs',
+							kids: ['Delete']
+						}]
+					}
+				]
+			}]
+ 		});
+ 		$("#list").append(li);
+ 	}
+ @@ -29,13 +40,9 @@ $(document).ready(function(){
+ 		event.preventDefault();
+ 	});
+ 
 
-	$(document).on("click", ".delete", function(e){
-		e.preventDefault();
-		$(this).parent().parent().parent().remove();
-	});	
-
-	var li = DOM.buildNode({
-    	kids: 'li'
-	});
-});
-
-
-
-
-
-
-// create a list item in js and set the text to the list item
-// $('<li/>')
-// how to create li in jq
-// append li to ul...?
+	$(".delete").click(function(){
+		$(li).remove();
+		// event.preventDefualt isn't needed 'cause this button doesn't submit a form
+ 	});
+ });
+ 
